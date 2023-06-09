7e69bef459216136f455c748a24658dec985f12a
fahim047 <fahimulislam58@gmail.com>
2023-06-09 08:48:11 +0600

    1335A-Candies  and Two Sisters

---
+++ Codeforces/A/1335A-Candies_and_Two_Sisters.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    int tc;
+    cin >> tc;
+    while(tc--){
+        ll n, cnt=0;
+        cin>> n;
+        cout << (n-1)/2 << endl;
+    }
+    return 0;
+}
