334881aeb996675ceaf813355b7f770e92fa448e
fahim047 <fahimulislam58@gmail.com>
2023-06-12 22:35:06 +0600

    492B - Vanya and Lanterns

--- Codeforces/B/492B-Vanya_and_Lanterns.cpp
+++ Codeforces/B/492B-Vanya_and_Lanterns.cpp
@@ -3,25 +3,23 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    int n, l, a[10005];
+    int n, l, a[10005], res;
     cin >> n >> l;
-
-    double res;
+    
     for(int i=0; i<n; i++){
         cin >> a[i];
     }
 
     sort(a,a+n);
 
-    res = max(a[0], l-a[n-1]);
+    res = 2 * max(a[0], l-a[n-1]);
 
     for(int i=0; i<n-1; i++){
-        res = max(res, (double)(a[i+1]-a[i])/2);
+        res = max(res, a[i+1]-a[i]);
     }
         
-    // res = 2.555555;
     // printf("%.10f\n", res);
-    cout << fixed << setprecision(10) << res << endl;
+    cout << fixed << setprecision(10) << (double)res/2 << endl;
 
     return 0;
 }
